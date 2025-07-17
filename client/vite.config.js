31c14144fc5d0741c3d536444f92df67f0997875
MohamedThoufiq27 <mohamedthoufiq981@gmail.com>
2025-07-17 12:39:53 +0530

    feat: Add vite-plugin-static-copy for handling static assets

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,10 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { viteStaticCopy } from 'vite-plugin-static-copy'  
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'public/_redirects',
+          dest: '.' // this ensures it lands in dist/_redirects
+        }
+      ]
+    })
+  ],
   assetsInclude: ['**/*.lottie'],
   server: {
     headers: {
@@ -12,7 +24,4 @@ export default defineConfig({
       "Cross-Origin-Embedder-Policy": "unsafe-none"
     }
   }
-});
-
-
-  
+})
